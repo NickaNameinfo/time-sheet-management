dfe789c030a39931219b831329b556dacf98c7ed
ArulKumar8270 <arulkumar8270@gmail.com>
2023-08-06 18:03:41 +0530

    data table and api update

--- front-end/src/Admin/Home.jsx
+++ front-end/src/Admin/Home.jsx
@@ -2,32 +2,10 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 
 function Home() {
-  const [adminCount, setAdminCount] = useState();
-  const [employeeCount, setEmployeeCount] = useState();
-  const [salary, setSalary] = useState();
+  const [adminCount, setAdminCount] = useState(null);
+  const [employeeCount, setEmployeeCount] = useState(null);
+  const [salary, setSalary] = useState(null);
 
-  useEffect(() => {
-    axios
-      .get("http://localhost:8081/adminCount")
-      .then((res) => {
-        setAdminCount(res.data[0].admin);
-      })
-      .catch((err) => console.log(err));
-
-    axios
-      .get("http://localhost:8081/employeeCount")
-      .then((res) => {
-        setEmployeeCount(res.data[0].employee);
-      })
-      .catch((err) => console.log(err));
-
-    axios
-      .get("http://localhost:8081/salary")
-      .then((res) => {
-        setSalary(res.data[0].sumOfSalary);
-      })
-      .catch((err) => console.log(err));
-  }, []);
   return (
     <div className="mainBody">
       <div className="mt-4">
@@ -73,79 +51,6 @@ function Home() {
             </div>
           </div>
         </div>
-      </div>
-      {/* List of admin  */}
-      <div className="mt-4">
-        {/* <h3 className="title">List of Projects</h3> */}
-        <table className="table">
-          <thead>
-            <tr>
-              <th>Project Name</th>
-              <th>Lead Name</th>
-              <th>Team Members</th>
-              <th>Project Start Date</th>
-              <th>Project End Date</th>
-              <th>Total Hours</th>
-              <th>Remaining Hours</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>Project 1</td>
-              <td>Wateer Supply</td>
-              <td>Admin</td>
-              <td>09/05/2023</td>
-              <td>08/011/2023</td>
-              <td>90</td>
-              <td>70</td>
-            </tr>
-            <tr>
-              <td>Project 1</td>
-              <td>Wateer Supply</td>
-              <td>Admin</td>
-              <td>09/05/2023</td>
-              <td>08/011/2023</td>
-              <td>90</td>
-              <td>70</td>
-            </tr>
-            <tr>
-              <td>Project 1</td>
-              <td>Wateer Supply</td>
-              <td>Admin</td>
-              <td>09/05/2023</td>
-              <td>08/011/2023</td>
-              <td>90</td>
-              <td>70</td>
-            </tr>
-            <tr>
-              <td>Project 1</td>
-              <td>Wateer Supply</td>
-              <td>Admin</td>
-              <td>09/05/2023</td>
-              <td>08/011/2023</td>
-              <td>90</td>
-              <td>70</td>
-            </tr>
-            <tr>
-              <td>Project 1</td>
-              <td>Wateer Supply</td>
-              <td>Admin</td>
-              <td>09/05/2023</td>
-              <td>08/011/2023</td>
-              <td>90</td>
-              <td>70</td>
-            </tr>
-            <tr>
-              <td>Project 1</td>
-              <td>Wateer Supply</td>
-              <td>Admin</td>
-              <td>09/05/2023</td>
-              <td>08/011/2023</td>
-              <td>90</td>
-              <td>70</td>
-            </tr>
-          </tbody>
-        </table>
       </div>
     </div>
   );
